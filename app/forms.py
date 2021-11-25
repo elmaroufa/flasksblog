b880faab69140fe714eaca59d9f56025579dbc83
elmaroufa <elmaroufa@gmail.com>
2021-11-25 15:23:27 +0100

    add register system and finish simple login

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms import validators
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo, ValidationError, Email
+from app.models import User
 
 class LogimForm(FlaskForm):
 
@@ -9,3 +10,24 @@ class LogimForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign in')
+
+
+class RegisterForm(FlaskForm):
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+                'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,15 @@
-from flask import render_template, redirect, flash, url_for
-from app import app
-from app.forms import LogimForm
+from flask import render_template, redirect, flash, url_for, request
+from werkzeug.urls import url_parse
+from app import app, db
+from app.forms import LogimForm, RegisterForm
+from app.models import User, Post
+from flask_login import current_user, login_user, logout_user, login_required
 
 @app.route('/')
+@login_required
 def index():
     user = {'username' : 'salyabbo'}
-    return render_template('base.html', user=user)
+    return render_template('index.html', user=user)
 
 
 @app.route('/user/<name>')
@@ -22,9 +26,45 @@ def internal_server_error(e):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LogimForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, 
-        form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        posts = Post.query.filter_by(id=user.id).all()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid user or password')
+            return redirect(url_for('login'))
+        login_user(user, remember = form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc !='':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', form=form, title = 'Sign in')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('COngratulation, you are new user')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Regiseter', form=form)
+
+
+
+
+
+
+
