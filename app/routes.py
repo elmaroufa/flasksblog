775fb028badd084206e9c4ea525d33a46be13755
elmaroufa <elmaroufa@gmail.com>
2022-06-20 19:28:42 +0100

    feature pagination for blog

--- app/routes.py
+++ app/routes.py
@@ -17,7 +17,11 @@ def index():
         db.session.commit()
         flash('Your post is live')
         return redirect(url_for('index'))
-    posts = current_user.followed_posts().all()
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
     return render_template('index.html', form=form, posts=posts)
 
 
@@ -141,5 +145,8 @@ def unfollow(username):
 
 @app.route('/explore')
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
     return render_template('index.html', title='Explore',posts=posts)
--- config.py
+++ config.py
@@ -14,3 +14,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['elmaroufa@gmail.com']
+    POSTS_PER_PAGE = 3
